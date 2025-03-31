77e8a83c06a8c0d53bd3da675590432a4c17c6d4
limaxel <170299654+limaxel@users.noreply.github.com>
2025-03-31 10:46:01 +0800

    add starter code

---
+++ App.java
@@ -0,0 +1,37 @@
+package org.example.CarMgmt;
+
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+
+import org.example.CarMgmt.BeforeLogin.*;
+
+public class App 
+{
+	public static void main(String[] args) {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Screen screen = null;
+        try {
+            screen = terminalFactory.createScreen();
+            screen.startScreen();
+
+            // Create the GUI based on our screen
+            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
+
+            // TODO: show BeforeLogin main menu
+            MainMenu.showMainMenu(gui);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (screen != null) {
+                try {
+                    screen.stopScreen();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
